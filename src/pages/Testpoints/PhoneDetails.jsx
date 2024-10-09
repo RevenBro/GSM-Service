eff04ff4f1dc839bf66e70d91595cd394b57a3af
RevenBro <saidakbarov077@gmail.com>
2024-10-09 22:32:07 +0500

    Just updated

--- src/pages/Testpoints/PhoneDetails.jsx
+++ src/pages/Testpoints/PhoneDetails.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import TestpointData from "../../../testpoints.json";
 
-const Testpoints = () => {
+const PhoneDetails = () => {
   const { brand } = useParams();
   const [models, setModels] = useState([]);
 
@@ -37,4 +37,4 @@ const Testpoints = () => {
   );
 };
 
-export default Testpoints;
+export default PhoneDetails;
--- src/pages/Testpoints/Testpoints.jsx
+++ src/pages/Testpoints/Testpoints.jsx
@@ -16,9 +16,9 @@ const Testpoints = () => {
       <div className="container mx-auto">
         <h2 className="text-4xl font-bold text-center mb-10">Test Points</h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
-          <TestpointCard onclick={() => handleBrandClick('samsung')} logo={SamsungLogo} brand={"Samsung"} body={"Mavjud modellar soni: 0"}/>
-          <TestpointCard onclick={() => handleBrandClick('redmi')} logo={RedmiLogo} brand={"Redmi"} body={"Mavjud modellar soni: 0"}/>
-          <TestpointCard onclick={() => handleBrandClick('huawei')} logo={HuaweiLogo} brand={"Huawei"} body={"Mavjud modellar soni: 0"}/>
+          <TestpointCard onclick={() => handleBrandClick('samsung')} logo={SamsungLogo} brand={"Samsung"} body={`Mavjud modellar soni: 0`}/>
+          <TestpointCard onclick={() => handleBrandClick('redmi')} logo={RedmiLogo} brand={"Redmi"} body={`Mavjud modellar soni: 0`}/>
+          <TestpointCard onclick={() => handleBrandClick('huawei')} logo={HuaweiLogo} brand={"Huawei"} body={`Mavjud modellar soni: 0`}/>
         </div>
       </div>
     </section>
