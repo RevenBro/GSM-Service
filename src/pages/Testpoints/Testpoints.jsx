0101f9b6500e5ff7e352b10375eb840145fa29f0
RevenBro <saidakbarov077@gmail.com>
2024-10-12 15:37:53 +0500

    Poco phone testpoints added

--- src/pages/Testpoints/Testpoints.jsx
+++ src/pages/Testpoints/Testpoints.jsx
@@ -3,6 +3,7 @@ import TestpointCard from "../../components/TestpointCard"
 import SamsungLogo from "../../assets/images/samsung-logo.svg"
 import RedmiLogo from "../../assets/images/redmi-logo.svg"
 import HuaweiLogo from "../../assets/images/huawei-logo.svg"
+import PocoLogo from "../../assets/images/poco-logo.png"
 import { useNavigate } from "react-router-dom";
 
 const Testpoints = () => {
@@ -17,7 +18,8 @@ const Testpoints = () => {
         <h2 className="text-4xl font-bold text-center mb-10">Test Points</h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
           <TestpointCard onclick={() => handleBrandClick('samsung')} logo={SamsungLogo} brand={"Samsung"} body={`Mavjud modellar soni: 0`}/>
-          <TestpointCard onclick={() => handleBrandClick('redmi')} logo={RedmiLogo} brand={"Redmi"} body={`Mavjud modellar soni: 0`}/>
+          <TestpointCard onclick={() => handleBrandClick('redmi')} logo={RedmiLogo} brand={"Redmi"} body={`Mavjud modellar soni: 44`}/>
+          <TestpointCard onclick={() => handleBrandClick('poco')} logo={PocoLogo} brand={"Poco"} body={`Mavjud modellar soni: 11`}/>
           <TestpointCard onclick={() => handleBrandClick('huawei')} logo={HuaweiLogo} brand={"Huawei"} body={`Mavjud modellar soni: 0`}/>
         </div>
       </div>
