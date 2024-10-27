e74a7be74bf9dbc84cf68e95efa832f75ca0ebc5
RevenBro <saidakbarov077@gmail.com>
2024-10-27 15:45:41 +0500

    Firmware page started

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ import Contact from './pages/Contact';
 import FAQ from './pages/FAQ';
 import TestPoints from './pages/Testpoints/Testpoints';
 import PhoneDetails from './pages/Testpoints/PhoneDetails';
+import Firmwares from "./pages/Firmwares/Firmwares"
+import FirmwareDetails from "./pages/Firmwares/FirmwareDetails"
 import i18n from './locale/i18n'; 
 import './locale/i18n'; 
 
@@ -20,11 +22,13 @@ function App() {
       <Header changeLang={changeLang} />
 
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/faq" element={<FAQ />} />
-        <Route path="/testpoints" element={<TestPoints />} />
-        <Route path="/testpoints/:brand" element={<PhoneDetails />} />
+        <Route path="/" element={<Home/>}/>
+        <Route path="/contact" element={<Contact/>}/>
+        <Route path="/faq" element={<FAQ/>}/>
+        <Route path="/testpoints" element={<TestPoints/>}/>
+        <Route path="/testpoints/:brand" element={<PhoneDetails/>}/>
+        <Route path='/firmwares' element={<Firmwares/>}/>
+        <Route path="/firmwares/:brand" element={<FirmwareDetails/>}/>
       </Routes>
 
       <Footer />
