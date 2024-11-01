30a8f35dcf50963450a6baf29133f429bf47645e
RevenBro <saidakbarov077@gmail.com>
2024-11-01 17:19:28 +0500

    Firmwares page started

---
+++ src/pages/Firmwares/FirmwareDetails.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function FirmwareDetails() {
+  return (
+    <div>FirmwareDetails</div>
+  )
+}
+
+export default FirmwareDetails
---
+++ src/pages/Firmwares/Firmwares.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Firmwares() {
+  return (
+    <div>Firmwares</div>
+  )
+}
+
+export default Firmwares
