3a29c5fe112c0d58f2529053ea3d792b3b22143b
RevenBro <saidakbarov077@gmail.com>
2024-11-28 15:01:17 +0500

    New Samsung models added

--- src/pages/Testpoints/Testpoints.jsx
+++ src/pages/Testpoints/Testpoints.jsx
@@ -17,7 +17,7 @@ const Testpoints = () => {
       <div className="container mx-auto">
         <h2 className="text-4xl font-bold text-center mb-10">Test Points</h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
-          <TestpointCard onclick={() => handleBrandClick('samsung')} logo={SamsungLogo} brand={"Samsung"} body={`Mavjud modellar soni: 0`}/>
+          <TestpointCard onclick={() => handleBrandClick('samsung')} logo={SamsungLogo} brand={"Samsung"} body={`Mavjud modellar soni: 16`}/>
           <TestpointCard onclick={() => handleBrandClick('redmi')} logo={RedmiLogo} brand={"Redmi"} body={`Mavjud modellar soni: 44`}/>
           <TestpointCard onclick={() => handleBrandClick('poco')} logo={PocoLogo} brand={"Poco"} body={`Mavjud modellar soni: 11`}/>
           <TestpointCard onclick={() => handleBrandClick('huawei')} logo={HuaweiLogo} brand={"Huawei"} body={`Mavjud modellar soni: 0`}/>
